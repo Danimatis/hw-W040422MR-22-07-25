a895016014e5a06017878f93ca243eb5f82ba92e
Dani Matis <danimatis@gmail.com>
2022-07-20 21:37:39 +0300

    complete-hopefully

--- script.js
+++ script.js
@@ -2,48 +2,69 @@ const userNameInput = document.getElementById("name");
 const userAgeInput = document.getElementById("age");
 const userCountryInput = document.getElementById("country");
 const userColorInput = document.getElementById("color");
-("users-backgroundColor");
+const genderMaleInput = document.getElementById("male");
+const genderFemaleInput = document.getElementById("female");
 const userDetailsOutput = document.getElementById("bootstrapCard");
-const userDetailsOutputNew = userDetailsOutput.lastChild;
-// another way to do this function is have return at the end of each function
-function clearInputs() {
-  userNameInput.value = "";
-  userAgeInput.value = "";
-  userCountryInput.value = "Israel";
-  userColorInput.value = "#000000";
-}
-function changeCardBackgroundColor() {
-  userDetailsOutput.lastChild.style.backgroundColor = userColorInput.value;
-}
 function displayUserInformationInCard() {
   if (userNameInput.value.length < 2) {
     alert("No Such Thing As A Letter being A Name! Try Again");
     return;
-  }
-  if (userAgeInput.value <= 0) {
+  } else if (userAgeInput.value <= 0) {
     alert("With Such An Age You Don't Exist Yet");
+    return;
+  } else if (assessUserGender() === undefined) {
+    alert("Please choose your gender- we are not politically correct here 😝");
+    return;
+  } else if (userCountryInput.value === "none") {
+    alert("Please tell us where you live");
     return;
   }
 
+  writeInnerHTML(
+    userNameInput.value,
+    userAgeInput.value,
+    userCountryInput.value,
+    userColorInput.value,
+    assessUserGender()
+  );
+  clearInputs(assessUserGender);
+}
+function assessUserGender() {
+  if (genderFemaleInput.checked === true) {
+    return "female";
+  } else if (genderMaleInput.checked === true) {
+    return "male";
+  } else {
+    return;
+  }
+}
+function writeInnerHTML(name, age, country, color, gender) {
   userDetailsOutput.innerHTML +=
-    `<div class="card me-3" style="width: 18rem; background-color: color"><div class="card-header" id="users-name style">Name: ` +
-    userNameInput.value +
+    `<div class="card me-3" style="width: 18rem; background-color:` +
+    color +
+    `"><div class="card-header" id="users-name style">Name: ` +
+    name +
     `</div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item" id="users-age">Age: ` +
-    userAgeInput.value +
+    age +
     `</li>
     <li class="list-group-item" id="users-country">Country: ` +
-    userCountryInput.value +
+    country +
     `</li>
     <li class="list-group-item" id="users-backgroundColor">You chose this color in hexadecimal: ` +
-    userColorInput.value +
+    color +
     `</li>
-    <li class="list-group-item" id="users-gender"></li>
+    <li class="list-group-item" id="users-gender"> You are a ` +
+    gender +
+    `</li>
   </ul></div>`;
-  changeCardBackgroundColor();
-  clearInputs();
 }
-
-// console.dir(document.getElementById("color"));
-// console.dir(userDetailsOutput.lastChild);
+function clearInputs() {
+  userNameInput.value = "";
+  userAgeInput.value = "";
+  userCountryInput.value = "none";
+  userColorInput.value = "#000000";
+  genderFemaleInput.checked = false;
+  genderMaleInput.checked = false;
+}
